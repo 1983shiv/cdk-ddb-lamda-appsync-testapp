35844aabf679b24f51297898af361f9dfeeea36c
shiv <1983.shiv@gmail.com>
2025-03-12 21:28:01 +0530

    created the first database stack basic config

---
+++ stacks/database-stack.ts
@@ -0,0 +1,20 @@
+import * as cdk from 'aws-cdk-lib';
+import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class DatabaseStack extends cdk.Stack {
+    public readonly usersTable: Table;
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+        this.usersTable = new Table(this, 'UserTable', {
+            partitionKey: {
+                name: 'UserID',
+                type: AttributeType.STRING
+            },
+            tableName: "Users",
+            billingMode: BillingMode.PAY_PER_REQUEST,
+            removalPolicy: cdk.RemovalPolicy.DESTROY
+        });
+  }
+}
