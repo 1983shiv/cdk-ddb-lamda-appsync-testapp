61eaef9d080fedf98c71b7139d3f6d150d479953
shiv <1983.shiv@gmail.com>
2025-03-18 21:38:22 +0530

    added appsync stack and schema for todos graphql api endpoint

---
+++ stacks/appsync-stack.ts
@@ -0,0 +1,51 @@
+import * as cdk from 'aws-cdk-lib';
+import * as awsAppsync from 'aws-cdk-lib/aws-appsync';
+import * as path from 'path';
+import { Construct } from 'constructs';
+import { aws_cognito } from 'aws-cdk-lib';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { UserPool } from 'aws-cdk-lib/aws-cognito';
+
+interface AppsyncStackProps extends cdk.StackProps {
+    userPool: UserPool;
+}
+
+export class AppsyncStack extends cdk.Stack {
+    public readonly api: awsAppsync.GraphqlApi;
+    constructor(scope: Construct, id: string, props: AppsyncStackProps) {
+        super(scope, id, props);
+        this.api = this.createAppsyncApi(props);
+    }
+
+    createAppsyncApi(props: AppsyncStackProps) {
+        const api = new awsAppsync.GraphqlApi(this, 'TodoAppsyncAPI', {
+            name: 'TodoAppsyncAPI',
+            definition: awsAppsync.Definition.fromFile(
+                path.join(__dirname, '../schema/schema.graphql')
+            ),
+            // schema: awsAppsync.SchemaFile.fromAsset(
+            //     path.join(__dirname, '../schema/schema.graphql')
+            // ),
+            authorizationConfig: {
+                defaultAuthorization: {
+                    authorizationType: awsAppsync.AuthorizationType.USER_POOL,
+                    userPoolConfig: {
+                        userPool: props.userPool,
+                    },
+                },
+                additionalAuthorizationModes: [
+                    {
+                        authorizationType: awsAppsync.AuthorizationType.IAM,
+                    },
+                ],
+            },
+            logConfig: {
+                fieldLogLevel: awsAppsync.FieldLogLevel.ALL,
+            },
+        });
+        new cdk.CfnOutput(this, 'GraphQLAPIURL', {
+            value: api.graphqlUrl,
+        });
+        return api;
+    }
+}
